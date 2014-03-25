d9528f575f226b8356b5fb53c0e16bcc386c14b3
LUQI ZHANG <zhangluqi16@gmail.com>
2014-03-26 00:14:21 +1100

    finish layout and rooutes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,7 +53,23 @@ describe "Static pages" do
 
     it "should not have a custom page title" do
       visit '/static_pages/about'
-      expect(page).not_to have_title('| About Us')
+      expect(page).not_to have_title('| About Us')  
+    end
+  end
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/contact'
+      expect(page).not_to have_title('| Contact')  
     end
   end
 end
